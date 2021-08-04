395236620c4682b34d35950f1bd76e2ac42f49da
Marco <antnyhern@gmail.com>
2021-08-03 23:51:50 -0500

    All proyect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import ReactPlayer from "react-player";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="contenedor" >
+      <ReactPlayer 
+      url='https://www.youtube.com/watch?v=E-YzQsAe5eE'
+      width='100%'
+      height='100%'
+      controls
+      playing
+      muted
+      playbackRate={1.75}
+      onEnded={() => alert('Terminói el video')}
+      className="react-player"
+      />
+      
     </div>
   );
 }
